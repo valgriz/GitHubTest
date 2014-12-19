9e415ce45c356ac95ff78ebcc0083da22cf48b95
Steven Marturano <steven.marturano@gmail.com>
2014-12-18 20:41:00 -0500

    Adding this on PC

--- src/com/valgriz/main/GitClass.java
+++ src/com/valgriz/main/GitClass.java
@@ -6,8 +6,15 @@ public class GitClass {
 	
 	}
 	
+	
+	public void gitThis(){
+		//I am editing this file on my PC, lets see if it shows up on my laptop when I push changes
+	}
+	
 	public String toString(){
 		return "I am doing this to see if I can get git to work!";
 	}
+	
+	
 
 }
